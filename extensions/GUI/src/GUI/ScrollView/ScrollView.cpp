e1db5d41063cf3deebe60ad2a42de7932258094a
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-02-05 22:04:26 +0100

    Add a function to test if a scroll view is currently scrolling.

--- extensions/GUI/src/GUI/ScrollView/ScrollView.cpp
+++ extensions/GUI/src/GUI/ScrollView/ScrollView.cpp
@@ -209,6 +209,22 @@ void ScrollView::setSwallowTouches(bool needSwallow)
     }
 }
 
+bool ScrollView::isScrolling() const
+{
+    const Vec2 position = _container->getPosition();
+    const Vec2 minInset = getMinInset();
+    const Vec2 maxInset = getMaxInset();
+
+    return ((fabsf(_scrollDistance.x) > SCROLL_DEACCEL_DIST)
+             || (fabsf(_scrollDistance.y) > SCROLL_DEACCEL_DIST))
+      && ((_direction != Direction::BOTH)
+           && (_direction != Direction::VERTICAL)
+           || (minInset.y < position.y) && (position.y < maxInset.y))
+      && ((_direction != Direction::BOTH)
+           && (_direction != Direction::HORIZONTAL)
+           || (minInset.x < position.x) && (position.x < maxInset.x));
+}
+
 void ScrollView::setContentOffset(Vec2 offset, bool animated /* = false*/)
 {
     if (animated)
@@ -404,7 +420,7 @@ void ScrollView::relocateContainer(bool animated)
     }
 }
 
-Vec2 ScrollView::maxContainerOffset()
+Vec2 ScrollView::maxContainerOffset() const
 {
     Point anchorPoint = _container->isIgnoreAnchorPointForPosition() ? Point::ZERO : _container->getAnchorPoint();
     float contW       = _container->getContentSize().width * _container->getScaleX();
@@ -413,7 +429,7 @@ Vec2 ScrollView::maxContainerOffset()
     return Vec2(anchorPoint.x * contW, anchorPoint.y * contH);
 }
 
-Vec2 ScrollView::minContainerOffset()
+Vec2 ScrollView::minContainerOffset() const
 {
     Point anchorPoint = _container->isIgnoreAnchorPointForPosition() ? Point::ZERO : _container->getAnchorPoint();
     float contW       = _container->getContentSize().width * _container->getScaleX();
@@ -431,20 +447,8 @@ void ScrollView::deaccelerateScrolling(float /*dt*/)
     }
 
     float newX, newY;
-    Vec2 maxInset, minInset;
 
     _container->setPosition(_container->getPosition() + _scrollDistance);
-
-    if (_bounceable)
-    {
-        maxInset = _maxInset;
-        minInset = _minInset;
-    }
-    else
-    {
-        maxInset = this->maxContainerOffset();
-        minInset = this->minContainerOffset();
-    }
 
     newX = _container->getPosition().x;
     newY = _container->getPosition().y;
@@ -452,11 +456,7 @@ void ScrollView::deaccelerateScrolling(float /*dt*/)
     _scrollDistance = _scrollDistance * SCROLL_DEACCEL_RATE;
     this->setContentOffset(Vec2(newX, newY));
 
-    if ((fabsf(_scrollDistance.x) <= SCROLL_DEACCEL_DIST && fabsf(_scrollDistance.y) <= SCROLL_DEACCEL_DIST) ||
-        ((_direction == Direction::BOTH || _direction == Direction::VERTICAL) &&
-         (newY >= maxInset.y || newY <= minInset.y)) ||
-        ((_direction == Direction::BOTH || _direction == Direction::HORIZONTAL) &&
-         (newX >= maxInset.x || newX <= minInset.x)))
+    if (!isScrolling())
     {
         this->unschedule(AX_SCHEDULE_SELECTOR(ScrollView::deaccelerateScrolling));
         this->relocateContainer(true);
@@ -486,6 +486,16 @@ void ScrollView::performedAnimatedScroll(float /*dt*/)
     {
         _delegate->scrollViewDidScroll(this);
     }
+}
+
+Vec2 ScrollView::getMinInset() const
+{
+    return _bounceable ? _minInset : minContainerOffset();
+}
+
+Vec2 ScrollView::getMaxInset() const
+{
+    return _bounceable ? _maxInset : maxContainerOffset();
 }
 
 const Size& ScrollView::getContentSize() const
@@ -608,7 +618,7 @@ void ScrollView::onBeforeDraw()
 
 void ScrollView::afterDraw()
 {
-    
+
      Director::getInstance()->getRenderer()->addCallbackCommand(AX_CALLBACK_0(ScrollView::onAfterDraw, this),
                                                                 _globalZOrder);
 }
@@ -623,14 +633,14 @@ void ScrollView::onAfterDraw()
     if (_clippingToBounds)
     {
         auto glview = Director::getInstance()->getGLView();
-        
+
         if (_scissorRestored) {//restore the parent's scissor rect
             glview->setScissorInPoints(_parentScissorRect.origin.x, _parentScissorRect.origin.y, _parentScissorRect.size.width, _parentScissorRect.size.height);
         }
         else {
             Director::getInstance()->getRenderer()->setScissorTest(false);
         }
-        
+
     }
 }
 
