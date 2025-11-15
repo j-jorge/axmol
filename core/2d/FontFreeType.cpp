ba9996eb9e58eb77bc8815c6872afabbfbaf2437
Julien Jorge <julien.jorge@stuff-o-matic.com>
2025-11-15 19:06:44 +0100

    Integrate optional profiling using Tracy.

--- core/2d/FontFreeType.cpp
+++ core/2d/FontFreeType.cpp
@@ -426,6 +426,8 @@ unsigned char* FontFreeType::getGlyphBitmap(char32_t charCode,
                                             int& xAdvance,
                                             FontFaceInfo** ppFallbackInfo)
 {
+    ZoneScoped;
+
     unsigned char* ret = nullptr;
 
     // @remark: glyphIndex=0 means charactor is mssing on current font face
@@ -475,15 +477,21 @@ unsigned char* FontFreeType::getGlyphBitmapByIndex(unsigned int glyphIndex,
                                                    Rect& outRect,
                                                    int& xAdvance)
 {
+    ZoneScoped;
+
     unsigned char* ret = nullptr;
 
     do
     {
-        if (FT_Load_Glyph(_fontFace, glyphIndex, FT_LOAD_RENDER | FT_LOAD_NO_AUTOHINT))
-            break;
+        {
+            ZoneScopedN("FT_Load_Glyph");
+            if (FT_Load_Glyph(_fontFace, glyphIndex, FT_LOAD_RENDER | FT_LOAD_NO_AUTOHINT))
+                break;
+        }
 
         if (_distanceFieldEnabled && _fontFace->glyph->bitmap.buffer)
         {
+            ZoneScopedN("FT_Render_Glyph");
             // Require freetype version > 2.11.0, because freetype 2.11.0 sdf has memory access bug, see:
             // https://gitlab.freedesktop.org/freetype/freetype/-/issues/1077
             FT_Render_Glyph(_fontFace->glyph, FT_Render_Mode::FT_RENDER_MODE_SDF);
@@ -503,6 +511,7 @@ unsigned char* FontFreeType::getGlyphBitmapByIndex(unsigned int glyphIndex,
 
         if (_outlineSize > 0 && outWidth > 0 && outHeight > 0)
         {
+            ZoneScopedN("Outline");
             auto copyBitmap = new unsigned char[outWidth * outHeight];
             memcpy(copyBitmap, ret, outWidth * outHeight * sizeof(unsigned char));
 
@@ -538,6 +547,7 @@ unsigned char* FontFreeType::getGlyphBitmapByIndex(unsigned int glyphIndex,
             unsigned char* blendImage = nullptr;
             if (blendWidth > 0 && blendHeight > 0)
             {
+                ZoneScopedN("Blend");
                 FT_Pos index, index2;
                 auto imageSize = blendWidth * blendHeight * 2;
                 blendImage     = new unsigned char[imageSize];
@@ -590,6 +600,8 @@ unsigned char* FontFreeType::getGlyphBitmapByIndex(unsigned int glyphIndex,
 
 unsigned char* FontFreeType::getGlyphBitmapWithOutline(unsigned int glyphIndex, FT_BBox& bbox)
 {
+    ZoneScoped;
+
     unsigned char* ret = nullptr;
     if (FT_Load_Glyph(_fontFace, glyphIndex, FT_LOAD_NO_BITMAP) == 0)
     {
--- core/2d/Label.cpp
+++ core/2d/Label.cpp
@@ -50,6 +50,7 @@
 #include "renderer/Shaders.h"
 #include "renderer/backend/ProgramState.h"
 #include "renderer/backend/ProgramStateRegistry.h"
+#include "Tracy.h"
 
 namespace ax
 {
@@ -1030,6 +1031,8 @@ void Label::setLineBreakWithoutSpace(bool breakWithoutSpace)
 
 void Label::updateLabelLetters()
 {
+    ZoneScoped;
+
     if (!_letters.empty())
     {
         Rect uvRect;
@@ -1095,6 +1098,8 @@ bool Label::alignText()
         setContentSize(Vec2::ZERO);
         return true;
     }
+
+    ZoneScoped;
 
     bool ret = true;
     do
@@ -1215,6 +1220,8 @@ bool Label::isLetterHorizontallyClamped(float letterPositionX, float letterWidth
 
 bool Label::updateQuads()
 {
+    ZoneScoped;
+
     bool ret = true;
     for (auto&& batchNode : _batchNodes)
     {
@@ -1327,7 +1334,7 @@ bool Label::updateTTFConfigInternal(unsigned int mods)
             updateShaderProgram();
         }
     }
-    
+
     if (_fontConfig.italics)
         this->enableItalics();
     if (_fontConfig.bold)
@@ -1358,6 +1365,8 @@ void Label::setBMFontSizeInternal(float fontSize)
 
 void Label::scaleFontSize(float fontSize)
 {
+    ZoneScoped;
+
     bool shouldUpdateContent = true;
     if (_currentLabelType == LabelType::TTF)
     {
@@ -1656,6 +1665,8 @@ void Label::disableEffect(LabelEffect effect)
 
 void Label::createSpriteForSystemFont(const FontDefinition& fontDef)
 {
+    ZoneScoped;
+
     _currentLabelType = LabelType::STRING_TEXTURE;
 
     auto texture = new Texture2D;
@@ -1682,6 +1693,8 @@ void Label::createSpriteForSystemFont(const FontDefinition& fontDef)
 
 void Label::createShadowSpriteForSystemFont(const FontDefinition& fontDef)
 {
+    ZoneScoped;
+
     if (!fontDef._stroke._strokeEnabled && fontDef._fontFillColor == _shadowColor3B &&
         (fontDef._fontAlpha == _shadowOpacity))
     {
@@ -1768,6 +1781,8 @@ void Label::clearTextures()
 
 void Label::updateContent()
 {
+    ZoneScoped;
+
     clearTextures();
     bool updateFinished = true;
 
@@ -1787,6 +1802,8 @@ void Label::updateContent()
 
     if (_lineDrawNode)
     {
+        ZoneScopedN("lineDrawNode");
+
         Color4B lineColor = Color4B(_displayedColor);
         if (_textColor != Color4B::WHITE && _textColor != lineColor)
             lineColor = _textColor;
@@ -1842,6 +1859,7 @@ void Label::updateContent()
         }
         else if (_textSprite)  // ...and is the logic for System fonts
         {
+            ZoneScopedN("textSprite");
             computeStringNumLines();
             const auto spriteSize = _textSprite->getContentSize();
 
@@ -2099,6 +2117,7 @@ void Label::draw(Renderer* renderer, const Mat4& transform, uint32_t flags)
     {
         return;
     }
+    ZoneScoped;
     // Don't do calculate the culling if the transform was not updated
     bool transformUpdated = flags & FLAGS_TRANSFORM_DIRTY;
 #if AX_USE_CULLING
@@ -2576,6 +2595,8 @@ void Label::updateColor()
         return;
     }
 
+    ZoneScoped;
+
     Color4B color4(_displayedColor.r, _displayedColor.g, _displayedColor.b, _displayedOpacity);
 
     // special opacity for premultiplied textures
@@ -2826,6 +2847,8 @@ void Label::updateLetterSpriteScale(Sprite* sprite)
 
 void Label::computeAlignmentOffset()
 {
+    ZoneScoped;
+
     _linesOffsetX.clear();
     switch (_hAlignment)
     {
@@ -3146,11 +3169,13 @@ bool Label::multilineTextWrap(bool breakOnChar, bool ignoreOverflow)
 
 bool Label::multilineTextWrapByWord(bool ignoreOverflow)
 {
+    ZoneScoped;
     return multilineTextWrap(false, ignoreOverflow);
 }
 
 bool Label::multilineTextWrapByChar(bool ignoreOverflow)
 {
+    ZoneScoped;
     return multilineTextWrap(true, ignoreOverflow);
 }
 
